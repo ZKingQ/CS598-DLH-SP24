9248d6704908f6e5b8b68c9cd0c256b9b67a2d23
Kaiqing <zkqhnfg@gmail.com>
2024-05-03 21:55:59 +0800

    add EarlyStopper

--- ae_models.py
+++ ae_models.py
@@ -2,6 +2,27 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class EarlyStopper:
+    def __init__(self, patience=5, delta=0.0):
+        self.patience = patience
+        self.delta = delta
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+
+    def __call__(self, loss):
+        if self.best_loss is None:
+            self.best_loss = loss
+        elif loss > self.best_loss + self.delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_loss = loss
+            self.counter = 0
+
+        return self.early_stop
+
 
 class ShallowAutoEncoder(nn.Module):
     def __init__(self, input_dim=28 * 28):
